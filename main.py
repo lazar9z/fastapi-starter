790b8357268a665e9a7ef45791b17b66d733e826
lazar9z <lazar.prokovic@outlook.com>
2025-01-06 12:33:58 +0100

    request body

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Body
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
-from pydantic import BaseModel
+from student import Student
 
 app = FastAPI()
 
@@ -22,3 +21,27 @@ async def index():
 @app.get("/hello/{name}")
 async def hello(name: str = Path(..., min_length=1, max_length=20), page: int = Path(..., ge=1)):
     return {"message": f"Hello {name}!, page={page}"}
+
+
+@app.post("/students")
+async def student_data(s1: Student):
+    # data = {
+    #     "id": 1,
+    #     "name": "test",
+    #     "subjects": ["test1", "test2"],
+    # }
+    # s1 = Student(**data)
+    # print(s1)
+    # o1 = s1.model_dump()
+    # print(o1)
+    return s1
+
+
+@app.post("/students2")
+async def student_data2(name: str = Body(...), marks: int = Body(...)):
+    return {"name": name, "marks": marks}
+
+
+@app.post("/students/{college}")
+async def student_data3(college: str, age: int, student: Student):
+    return {"college": college, "age": age, **student.model_dump()}
---
+++ student.py
@@ -0,0 +1,8 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class Student(BaseModel):
+    id: int
+    name: str
+    subjects: List[str] = []
